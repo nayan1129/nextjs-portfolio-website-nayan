902122a27cc1a259cfebfe1645ddd127c36140ab
Nayan Lodha <nayanlodha1122@gmail.com>
2023-09-20 13:06:25 +0530

    contact form updated

--- src/app/Components/Contact.jsx
+++ src/app/Components/Contact.jsx
@@ -8,11 +8,15 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useForm } from "@formspree/react";
 
 const Contact = () => {
   useEffect(() => {
     AOS.init();
   }, []);
+
+  const [state, handleSubmit] = useForm("xwkdrbqw");
+  
 
   return (
     <div id="contact" className="w-full lg:h-screen">
@@ -21,8 +25,11 @@ const Contact = () => {
           Contact
         </p>
         <h2 className="py-4">Get In Touch</h2>
-        <div  className="grid lg:grid-cols-5 gap-8">
-          <div data-aos='zoom-in-down' className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4">
+        <div className="grid lg:grid-cols-5 gap-8">
+          <div
+            data-aos="zoom-in-down"
+            className="col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4"
+          >
             <div className="lg:p-4 h-full ">
               <div>
                 <Image
@@ -34,7 +41,9 @@ const Contact = () => {
                 />
               </div>
               <div>
-                <h2 className="py-2" style={{color:'#007bff'}} >Nayan Lodha</h2>
+                <h2 className="py-2" style={{ color: "#007bff" }}>
+                  Nayan Lodha
+                </h2>
                 <p>Front-End Developer</p>
                 <p className="py-4">
                   I am available for full-time positions. Contact me and
@@ -80,9 +89,12 @@ const Contact = () => {
             </div>
           </div>
 
-          <div data-aos='zoom-in-down' className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+          <div
+            data-aos="zoom-in-down"
+            className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4"
+          >
             <div className="p-4">
-              <form>
+              <form method="POST" action='https://formspree.io/f/xwkdrbqw' onSubmit={handleSubmit}>
                 <div className="grid md:grid-cols-2 gap-4 w-full py-2">
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Name</label>
@@ -90,16 +102,18 @@ const Contact = () => {
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
                       name="name"
+                      
                     />
                   </div>
                   <div className="flex flex-col">
-                    <label className="uppercase text-sm py-2">
+                    <label className="uppercase text-sm py-2" >
                       Phone Number
                     </label>
                     <input
                       className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
                       name="phone"
+                      
                     />
                   </div>
                 </div>
@@ -109,10 +123,13 @@ const Contact = () => {
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="email"
                     name="email"
+                    
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Subject</label>
+                  <label className="uppercase text-sm py-2" >
+                    Subject
+                  </label>
                   <input
                     className="border-2 rounded-lg p-3 flex border-gray-300"
                     type="text"
@@ -120,14 +137,16 @@ const Contact = () => {
                   />
                 </div>
                 <div className="flex flex-col py-2">
-                  <label className="uppercase text-sm py-2">Message</label>
+                  <label className="uppercase text-sm py-2" >
+                    Message
+                  </label>
                   <textarea
                     className="border-2 rounded-lg p-3 border-gray-300"
                     rows="10"
                     name="message"
                   ></textarea>
                 </div>
-                <button className="w-full p-4 text-gray-100 mt-4">
+                <button  disabled={state.submitting} className="w-full p-4 text-gray-100 mt-4">
                   Send Message
                 </button>
               </form>
